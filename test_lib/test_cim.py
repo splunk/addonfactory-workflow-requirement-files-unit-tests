aaea90fdcb90f30a8a408c023fdbf24a15c65f30
Nandini Vij <nvij@splunk.com>
2021-12-14 13:21:24 -0800

    added cim version in error message

--- test_lib/test_cim.py
+++ test_lib/test_cim.py
@@ -1,18 +1,3 @@
-#   ########################################################################
-#   Copyright 2021 Splunk Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-#   #######################################################################
 # Test to check if XML contains all cim recommended fields based on the model name of the event
 # Usage- test_cim.py -i <input log folder to test>
 import json
@@ -257,6 +242,7 @@ def cim_matching(file_name, jsonpath):
                     JENKINS_STATUS = False
                     logger.error("----------------------------------------------")
                     logger.error("FILENAME : " + str(file_name))
+                    logger.error("CIM VERSION :" + str(jsondir))
                     logger.error("EVENT : " + str(event))
                     logger.error("MODEL : " + str(model))
                     if dataset:
