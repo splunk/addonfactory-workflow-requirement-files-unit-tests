ee8a7fe88eb9427b8481b86bba519a8b64c0f288
Artem Rys <arys@splunk.com>
2021-12-20 23:13:40 +0100

    refactor: use pre-commit

--- test_lib/test_transport_attrib.py
+++ test_lib/test_transport_attrib.py
@@ -13,22 +13,23 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #   #######################################################################
+import argparse
+import logging
 import os
 import sys
-import argparse
-from lxml import etree
 from xml.etree import cElementTree as ET
-import logging
+
+from lxml import etree
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-output_file_handler = logging.FileHandler("test_transport_params_output.txt",mode='w')
+output_file_handler = logging.FileHandler("test_transport_params_output.txt", mode="w")
 stdout_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(output_file_handler)
 logger.addHandler(stdout_handler)
 INVALID = False
 
-#fetches root
+# fetches root
 def fetch_root_xml(file_name):
     root = None
     try:
@@ -38,50 +39,73 @@ def fetch_root_xml(file_name):
         pass
     return root
 
-#fetches transport type
-def extract_transport_tag(event):
-    for transport in event.iter('transport'):
-        return transport.get('type')
 
-#Checks for host source source_type
+# fetches transport type
+def extract_transport_tag(event):
+    for transport in event.iter("transport"):
+        return transport.get("type")
+
+
+# Checks for host source source_type
 def check_non_empty_params(event):
-    host, source,source_type = "", "",  ""
-    for transport in event.iter('transport'):
-        if transport.get('host') :
-            host = transport.get('host')
-        if transport.get('source') :
-            source = transport.get('source')
-        if transport.get('sourcetype') :
-            source_type = transport.get('sourcetype')
+    host, source, source_type = "", "", ""
+    for transport in event.iter("transport"):
+        if transport.get("host"):
+            host = transport.get("host")
+        if transport.get("source"):
+            source = transport.get("source")
+        if transport.get("sourcetype"):
+            source_type = transport.get("sourcetype")
     return host, source, source_type
 
-#main transport param function
+
+# main transport param function
 def check_transport_params(filename):
     try:
         etree.parse(filename)
     # check for file IO error
-    except IOError:
-        print('Invalid File')
+    except OSError:
+        print("Invalid File")
     except etree.XMLSyntaxError:
-        print('XML Syntax Error')
+        print("XML Syntax Error")
         quit()
     root = fetch_root_xml(filename)
     if root is None:
-        logger.debug("ERROR parsing xml file" + filename + '\n')
+        logger.debug("ERROR parsing xml file" + filename + "\n")
         return
-    transport_type_list = ["dbx", "modinput","Modinput", "Mod input","Modular Input", "Modular input", "modular input","modular_input", "Mod Input", "windows_input", "hec_event", "forwarder", "file_monitor", "scripted_input","scripted input","hec_raw" ]
+    transport_type_list = [
+        "dbx",
+        "modinput",
+        "Modinput",
+        "Mod input",
+        "Modular Input",
+        "Modular input",
+        "modular input",
+        "modular_input",
+        "Mod Input",
+        "windows_input",
+        "hec_event",
+        "forwarder",
+        "file_monitor",
+        "scripted_input",
+        "scripted input",
+        "hec_raw",
+    ]
     event_no = 1
-    for event in root.iter('event'):
+    for event in root.iter("event"):
         transport_type = extract_transport_tag(event)
         if transport_type in transport_type_list:
-            host, source, source_type =  check_non_empty_params(event)
-            if not( host and source and source_type):
-                logger.error(f"ERROR: Filename:{filename} Event_no:{event_no} with transport_type:{transport_type} has missing value.\nhost:\"{host}\" source:\"{source}\" or sourcetype:\"{source_type}\"")
+            host, source, source_type = check_non_empty_params(event)
+            if not (host and source and source_type):
+                logger.error(
+                    f'ERROR: Filename:{filename} Event_no:{event_no} with transport_type:{transport_type} has missing value.\nhost:"{host}" source:"{source}" or sourcetype:"{source_type}"'
+                )
                 global INVALID
-                INVALID= True
+                INVALID = True
         event_no += 1
 
-#input parsing
+
+# input parsing
 def parse_input(input_arg):
     if os.path.exists(input_arg):
         if os.path.isfile(input_arg):
@@ -99,9 +123,12 @@ def parse_input(input_arg):
     if INVALID:
         exit(1)
 
+
 if __name__ == "__main__":
-    print('Running transport attrib test:')
+    print("Running transport attrib test:")
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', help='Input-file/folder to test tranport params', required=True)
+    parser.add_argument(
+        "-i", "--input", help="Input-file/folder to test tranport params", required=True
+    )
     args = parser.parse_args()
     parse_input(args.input)
