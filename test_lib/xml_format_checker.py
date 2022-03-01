14c3a81c57b40f0719b6df4cb6a5e668db272375
Nandini Vij <nvij@splunk.com>
2022-03-01 13:59:04 -0800

    Added check for no files in directory

--- test_lib/xml_format_checker.py
+++ test_lib/xml_format_checker.py
@@ -36,6 +36,9 @@ def parsefile(file):
 def check_xml_format(input_folder):
     for dirpath, dirs, files in os.walk(input_folder):
         contains_log, contains_XML, test_status = False, False, True
+        if not os.listdir(dirpath):
+            print("No files in the directory.")
+            exit(1)
         for filename in files:
             fname = os.path.join(dirpath, filename)
             if fname.endswith(".log") or fname.endswith(".xml"):
